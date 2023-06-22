e1a2366462638ed500777ef8a24f3844e19f97e4
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-06-22 18:29:56 +0500

    nth

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,7 @@
-#Create a variable x and assign it the value of 10.
-
-#Create a variable y and assign it the value of 5.
-
+x=int(input()) #Create a variable x and assign it the value of 10.
+y=int(input())
+sum=x+y #Create a variable y and assign it the value of 5.
+print(sum)
 #Create a variable result and assign it the value of x + y.
 
 #Print the value of result.
